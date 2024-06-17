bba07817063dd5b1c36039b241237d334fe628dc
Gerald Zandisko <imgerraour@gmail.com>
2024-06-17 07:35:29 +0700

    Add trading view widgets

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import ReactDOM from 'react-dom/client';
 import WebHead from './components/WebHead';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { TickerTape } from "react-ts-tradingview-widgets";
 
 function App() {
   return (
-    <div>
-      <WebHead />
-      <h1>Hallo</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <WebHead />
+          <h1>Hallo</h1>
+          <TickerTape colorTheme="dark"></TickerTape>
+        </div>
+      </div>
     </div>
   );
 }
