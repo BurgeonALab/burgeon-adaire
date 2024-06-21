6c9902755a69e9ed944cf8d521c792cbc18ef5c1
Gerald Zandisko <imgerraour@gmail.com>
2024-06-21 09:07:19 +0700

    add navbar & footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import $ from 'jquery';
+
+export default class Navbar extends Component {
+  // jQuery = () => {
+  // Insert jQuery Code Here
+  // }
+
+  // componentDidMount() {
+  //   this.jQuery()
+  // }
+
+  render() {
+    return (
+      <div>
+        <p>This is Navbar!</p>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+// React
 import ReactDOM from 'react-dom/client';
-import WebHead from './components/WebHead';
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+// CSS
+import './css/style.css'
+// Components
 import { TickerTape } from "react-ts-tradingview-widgets";
+import WebHead from './components/WebHead';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="container-fluid">
+    <div className="bottom-footer-container container-fluid">
       <div className="row">
         <div className="col-md-12">
           <WebHead />
+          <Navbar />
           <h1>Hallo</h1>
           <TickerTape colorTheme="dark"></TickerTape>
         </div>
+      </div>
+      <div class="footer">
+        <p className="text-center">This is footer!</p>
       </div>
     </div>
   );
