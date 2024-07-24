421f41100d00d724a4d475fb12f74126f144e2fe
Gerald Zandisko <imgerraour@gmail.com>
2024-07-24 19:43:25 +0700

    updates

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,12 +4,17 @@ export default class BAIJumbotron extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row about-container">
-          <div className="col-md-6 about-items p-3">
-            <div className="about-contents rounded"></div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="about-box py-3">
+              <div className="about-item rounded">
+              </div>
+            </div>
           </div>
-          <div className="col-md-6 about-items p-3">
-            <div className="about-contents rounded"></div>
+          <div className="col-md-6">
+            <div className="about-box py-3">
+              <div className="about-item rounded"></div>
+            </div>
           </div>
         </div>
       </div>
