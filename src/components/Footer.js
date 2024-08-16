98a54987bb96756ceea7052643cf28cb4c24cac8
Gerald Zandisko <imgerraour@gmail.com>
2024-08-17 00:49:09 +0700

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ export default class Footer extends Component {
         <div>
           <div className="container-fluid py-5">
             <div className="row">
-              <div className="burgeon-partner-major col-md-6 d-flex justify-content-start align-items-center">
+              <div className="burgeon-partner-major col-lg-6 d-flex justify-content-start align-items-center">
                 <img className='partner-pepperstone' alt="Pepperstone Official Partner" src={BurgeonAdairePepperstone} height={80}></img>
               </div>
-              <div className="burgeon-partner-major col-md-6 d-flex justify-content-end align-items-center">
+              <div className="burgeon-partner-major col-lg-6 d-flex justify-content-end align-items-center">
                 <img className='partner-aws' src="https://d0.awsstatic.com/logos/powered-by-aws-white.png" height={56} alt="Powered by AWS Cloud Computing"></img>
               </div>
             </div>
