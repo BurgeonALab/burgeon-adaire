5529714a9479f3db68e9fee686e91c5684fbc896
Gerald Zandisko <imgerraour@gmail.com>
2024-08-18 00:02:43 +0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   BAILogo,
 } from './images';
+import { MaterialSymbol } from 'react-material-symbols';
 // import $ from 'jquery';
 
 export default class Navbar extends Component {
@@ -24,28 +25,45 @@ export default class Navbar extends Component {
               </a>
               <span className='burgeon-nav-title ms-3 d-flex align-items-center'><strong>Burgeon Adaire International</strong></span>
             </div>
-            <div className='burgeon-nav-menu w-50 d-flex flex-row-reverse'>
-              <ul className="nav d-flex me-3">
-                <li className="nav-item ms-3 d-flex align-items-center">
-                  <a>Identity</a>
-                </li>
-                <li className="nav-item ms-3 d-flex align-items-center">
-                  <a>Operations</a>
-                </li>
-                <li className="nav-item ms-3 d-flex align-items-center">
-                  <a>Gratitude</a>
-                </li>
-                <li className="nav-item ms-3 d-flex align-items-center">
-                  <a className='nav-link dropdown-toggle p-0 link-dark' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                    Legal
-                  </a>
-                  <ul className='dropdown-menu dropdown-menu-end'>
-                    <li><a className='dropdown-item' href='#'>Privacy Policy</a></li>
-                    <li><a className='dropdown-item' href='#'>Cookie Policy</a></li>
-                    <li><a className='dropdown-item' href='#'>Terms & Conditions</a></li>
-                  </ul>
-                </li>
-              </ul>
+            <div className='w-50 d-flex flex-row-reverse'>
+              <div className='burgeon-nav-mobile-button'>
+                <button type='button' data-bs-toggle='offcanvas' data-bs-target='#burgeonNavbarMobile' aria-controls='burgeonNavbarMobile' className='btn btn-link d-flex align-items-center justify-content-center text-decoration-none'>
+                  <MaterialSymbol icon="menu" size={24} fill grade={-25} color='#000000' />
+                </button>
+                <div className='offcanvas offcanvas-end' data-bs-scroll='true' tabIndex={-1} id='burgeonNavbarMobile' aria-labelledby='burgeonNavbarMobileLabel'>
+                  <div className='offcanvas-header d-flex justify-content-between flex-row'>
+                    <h5 className='offcanvas-title' id='burgeonNavbarMobileLabel'>PT. Burgeon Adaire International</h5>
+                    <button type='button' className='btn btn-link d-flex align-items-center justify-content-center text-decoration-none' data-bs-dismiss='offcanvas' aria-label='Close'>
+                      <MaterialSymbol icon="close" size={24} fill grade={-25} color='#000000' />
+                    </button>
+                  </div>
+                  <div className='offcanvas-body'>
+                  </div>
+                </div>
+              </div>
+              <div className='burgeon-nav-menu'>
+                <ul className="nav d-flex me-3">
+                  <li className="nav-item ms-3 d-flex align-items-center">
+                    <a>Identity</a>
+                  </li>
+                  <li className="nav-item ms-3 d-flex align-items-center">
+                    <a>Operations</a>
+                  </li>
+                  <li className="nav-item ms-3 d-flex align-items-center">
+                    <a>Gratitude</a>
+                  </li>
+                  <li className="nav-item ms-3 d-flex align-items-center">
+                    <a className='nav-link dropdown-toggle p-0 link-dark' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                      Legal
+                    </a>
+                    <ul className='dropdown-menu dropdown-menu-end'>
+                      <li><a className='dropdown-item' href='#'>Privacy Policy</a></li>
+                      <li><a className='dropdown-item' href='#'>Cookie Policy</a></li>
+                      <li><a className='dropdown-item' href='#'>Terms & Conditions</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
