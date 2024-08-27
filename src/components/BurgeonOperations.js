8959f675417b01f8b090a6492679b5f5dc85cc43
Gerald Zandisko <imgerraour@gmail.com>
2024-08-27 20:00:51 +0700

    updates

--- src/components/BurgeonOperations.js
+++ src/components/BurgeonOperations.js
@@ -7,6 +7,10 @@ export default class BurgeonOperations extends Component {
 			<div className='container-fluid py-3'>
 				<div className='row'>
 					<div className='col-md-12'>
+						<div className='operation-text-center'>
+							<h2 className='text-white'>Operation</h2>
+							<p className='lead text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
 						<TickerTape colorTheme="dark"></TickerTape>
 					</div>
 				</div>
