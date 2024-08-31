ea41144207795a27993aac8fd3fa241bbd7f50fd
Gerald Zandisko <imgerraour@gmail.com>
2024-08-31 11:40:31 +0700

    updates

--- src/components/BurgeonOperations.js
+++ src/components/BurgeonOperations.js
@@ -1,61 +1,29 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faChevronLeft,
-	faChevronRight,
-} from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+// 	faChevronLeft,
+// 	faChevronRight,
+// } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import { TickerTape } from "react-ts-tradingview-widgets";
-import $ from 'jquery';
+// import $ from 'jquery';
 
 export default class BurgeonOperations extends Component {
-	jQuery = () => {
-		$(function () {
-			$("#operation-button").on("click", function () {
-				$(".operation-hide-overlay").css('justify-content', 'center');
-				$(".operation-hide-box").css('pointer-events', 'none');
-				$(".operation-hide-box").css('width', 'auto');
-				$(".button-operation-left").css('display', 'none');
-				$(".button-operation-right").css('display', 'block');
-				$(".operation-description-minimize").css('display', 'none');
-			});
-			$("#operation-button-expand").on("click", function () {
-				$(".operation-hide-box").css('width', 'revert');
-				$(".operation-description-minimize").css('display', 'block');
-				$(".button-operation-right").css('display', 'none');
-				$(".button-operation-left").css('display', 'block');
-				$(".operation-hide-box").css('pointer-events', 'auto');
-			});
-		});
-	}
+	// jQuery = () => {
+	// }
 
 	componentDidMount() {
-		this.jQuery()
+		// this.jQuery()
 	}
 	render() {
 		return (
 			<div className='container-fluid py-3' >
-				<div className='col-md-12 operation-hide-box py-3'>
-					<div className='operation-hide-overlay operation-text-center me-5'>
-						<div className='d-flex flex-row overlay-content-mobile'>
-							<div className='operation-description-minimize padding-24'>
-								<h2 className='text-white'>Operation</h2>
-								<p className='lead text-white mb-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-							</div>
-							<div className='flex-shrink-1 align-self-center padding-24'>
-								<a id="operation-button" className='button-operation-left' role='button'>
-									<FontAwesomeIcon id="chevron-overlay" icon={faChevronLeft} className='link-light' size='lg' />
-								</a>
-								<a id="operation-button-expand" className='button-operation-right' role='button'>
-									<FontAwesomeIcon id="chevron-overlay" icon={faChevronRight} className='link-light' size='lg' />
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
+				{/* <div className='col-md-12 operation-container py-3'>
+				</div> */}
 				<div className='row operations-container'>
-					<div className='col-md-4 py-3'>
+					<div className='col-xxl-3 py-3'>
+						<div className='adairesolutions-box rounded'></div>
 					</div>
-					<div className='col-md-8 d-flex flex-column py-3'>
+					<div className='col-xxl-9 d-flex flex-column py-3'>
 						<div>
 							<TickerTape
 								symbols={[
@@ -100,8 +68,19 @@ export default class BurgeonOperations extends Component {
 							>
 							</TickerTape>
 						</div>
-						<div className='flex-grow-1'></div>
-						<div className='flex-grow-1'></div>
+						<div className='flex-grow-1 operation-child-container'>
+							<div className='row h-100 portfolio-box-mobile'>
+								<div className='col-md-6'>
+									<div className='portfolio-box rounded'></div>
+								</div>
+								<div className='col-md-6'>
+									<div className='portfolio-box rounded'></div>
+								</div>
+							</div>
+						</div>
+						<div className='flex-grow-1 operation-child-container'>
+							<div className='ecommerce-box rounded'></div>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/BurgeonPartners.js
+++ src/components/BurgeonPartners.js
@@ -12,10 +12,10 @@ export default class BurgeonOutro extends Component {
     return (
       <div className='container-fluid'>
         <div className='row'>
-          <div className='col-md-12'>
+          <div className='col-md-12 py-3'>
             <div className='gratitude-text-center'>
               <h2 className='text-white'>Gratitude</h2>
-              <p className='lead text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className='lead text-white mb-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
           </div>
           <div className='col-md-6 py-3'>
