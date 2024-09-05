06756f00aaf54973fd605ae206b151c3fc35ac1d
Gerald Zandisko <imgerraour@gmail.com>
2024-09-05 09:09:51 +0700

    updates

--- src/components/BurgeonAbstract.js
+++ src/components/BurgeonAbstract.js
@@ -12,9 +12,8 @@ export default class BurgeonAbstract extends Component {
 						</h1>
 						<p className='lead text-white'><strong>PT. Burgeon Adaire International</strong> thrives at the intersection of innovation and opportunity, where we weave knowledge and growth into the fabric of an ever-changing landscape. We build connections that matter, creating spaces for exploration and collaboration. Beyond our primary pursuits, we extend our reach into new avenues, offering value and excellence in unexpected places.</p>
 					</div>
-					<div className='offset-xxl-1 col-xxl-3'>
-						<div className='sky-video-box py-3'>
-							<div className='sky-video-content rounded'></div>
+					<div className='sky-video-box-mobile offset-xxl-1 col-xxl-3'>
+						<div className='sky-video-content rounded'>
 						</div>
 					</div>
 				</div>
