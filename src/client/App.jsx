472a6d1f0dc3583cbe775665e5ed8587e5cb916e
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-10-18 20:35:59 +0700

    ssr route fix

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import {
-  BurgeonSSRHome,
-  BurgeonSSRPolicy,
-  BurgeonSSRCookie,
-  BurgeonSSRTAC,
-} from "../ssr-pages";
+import BurgeonSSRHomeMain from '../ssr-main/BurgeonSSRHomeMain';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<BurgeonSSRHome />} />
-      <Route path="/privacy-policy" element={<BurgeonSSRPolicy />} />
-      <Route path="/cookie-policy" element={<BurgeonSSRCookie />} />
-      <Route path="/terms-and-conditions" element={<BurgeonSSRTAC />} />
-    </Routes>
+    <html lang="en">
+      <head>
+        <meta property="og:site_name" content="PT. Burgeon Adaire International" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="canoncial" href="https://burgeonadaire.com" />
+        <link rel='stylesheet' type='text/css' charSet='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
+        <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body style={{ visibility: 'hidden', backgroundColor: '#121217' }}>
+        <BurgeonSSRHomeMain />
+      </body>
+    </html>
   );
 }
 
---
+++ src/ssr-main/BurgeonSSRHomeMain.js
@@ -0,0 +1,22 @@
+import React,
+{ Component } from 'react';
+import { Route, Routes } from "react-router-dom";
+import {
+  BurgeonSSRHome,
+  BurgeonSSRPolicy,
+  BurgeonSSRCookie,
+  BurgeonSSRTAC,
+} from "../ssr-pages";
+
+export default class BurgeonSSRHomeMain extends Component {
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<BurgeonSSRHome />} />
+        <Route path="/cookie-policy" element={<BurgeonSSRCookie />} />
+        <Route path="/privacy-policy" element={<BurgeonSSRPolicy />} />
+        <Route path="/terms-and-conditions" element={<BurgeonSSRTAC />} />
+      </Routes>
+    );
+  }
+}
