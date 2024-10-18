d0fca8e6c23bbf3fd4a2d9fb933bc68a4d6f3984
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-10-18 20:36:47 +0700

    components changes

--- src/ssr-pages/BurgeonSSRCookie.js
+++ src/ssr-pages/BurgeonSSRCookie.js
@@ -1,26 +1,21 @@
 import React, {
   Component,
 } from 'react';
+import {
+  Helmet,
+  HelmetProvider,
+} from 'react-helmet-async';
+
+const helmetContext = {};
 
 export default class BurgeonSSRCookie extends Component {
   render() {
     return (
-      <html>
-        <head>
-          <title>Cookie Policy | PT. Burgeon Adaire Solutions</title>
-          <meta name="description" content="PT. Burgeon Adaire International thrives in a dynamic landscape, offering insights and opportunities while building strong partnerships and consistently delivering exceptional value." />
-          <meta property="og:site_name" content="PT. Burgeon Adaire International" />
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="canoncial" href="https://burgeonadaire.com" />
-          <link rel='stylesheet' type='text/css' charSet='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
-          <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
-          <link rel="icon" href="/favicon.ico" />
-        </head>
-        <body>
-        </body>
-      </html>
+      <HelmetProvider context={helmetContext}>
+        <Helmet>
+          <title>Cookie Policy | PT. Burgeon Adaire International</title>
+        </Helmet>
+      </HelmetProvider>
     );
   }
 }
