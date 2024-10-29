470d4772b00ff933483a554a2b54432320637a1c
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-10-29 16:40:52 +0700

    add WebSite schema

--- src/components/BurgeonSchema.js
+++ src/components/BurgeonSchema.js
@@ -1,26 +1,36 @@
 var schema = {
   "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "Burgeon Adaire International",
-  "legalName": "PT. Burgeon Adaire International",
-  "url": "https://burgeonadaire.com",
-  "foundingDate": "2018",
-  "founders": [
+  "@graph": [
     {
-      "@type": "Person",
-      "name": "Gerald Zandisko"
+      "@type": "Organization",
+      "name": "Burgeon Adaire International",
+      "legalName": "PT. Burgeon Adaire International",
+      "url": "https://burgeonadaire.com",
+      "foundingDate": "2018",
+      "founders": [
+        {
+          "@type": "Person",
+          "name": "Gerald Zandisko"
+        },
+        {
+          "@type": "Person",
+          "name": "Nadine Huang"
+        }
+      ],
+      "sponsor":
+      {
+        "@type": "Organization",
+        "name": "Pepperstone",
+        "url": "https://pepperstone.com"
+      }
     },
     {
-      "@type": "Person",
-      "name": "Nadine Huang"
+      "@type": "WebSite",
+      "name": "PT. Burgeon Adaire International",
+      "alternateName": "Burgeon Adaire International",
+      "url": "https://burgeonadaire.com"
     }
   ],
-  "sponsor":
-  {
-    "@type": "Organization",
-    "name": "Pepperstone",
-    "url": "https://pepperstone.com"
-  }
 }
 
 var script = document.createElement('script');
