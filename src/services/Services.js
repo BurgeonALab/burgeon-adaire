fcaa5fc090aad67471be2909d75a18449febee7e
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-11-23 13:52:12 +0700

    hide keys (apparently)

--- src/services/Services.js
+++ src/services/Services.js
@@ -5,13 +5,13 @@ import {
 } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkfkF79ZkgZSAh2vvF-Y1vh5Um5ZYMUWY",
-  authDomain: "burgeon-adaire.firebaseapp.com",
-  projectId: "burgeon-adaire",
-  storageBucket: "burgeon-adaire.appspot.com",
-  messagingSenderId: "764018528295",
-  appId: "1:764018528295:web:2ae25591e22d167e1efe02",
-  measurementId: "G-VBWQHXE96E"
+  apiKey: process.env.FIREBASE_APP_API_KEY,
+  authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_APP_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
