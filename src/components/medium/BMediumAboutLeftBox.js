8eacf5c8cb0568317cc6db4aac5cd0a80ab8ce37
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-11-26 15:56:12 +0700

    about show text on mobile

--- src/components/medium/BMediumAboutLeftBox.js
+++ src/components/medium/BMediumAboutLeftBox.js
@@ -11,8 +11,8 @@ export default class BMediumAboutLeftBox extends Component {
           <div className="about-item about-item-blue rounded p-4">
             <div className='h-100 position-relative'>
               <div className='d-flex flex-column justify-content-center h-100'>
-                <h3 className='about-leftbox-header-mobile text-light text-center'>Rosa Parks</h3>
-                <p className='about-leftbox-description-mobile text-light text-center mb-0'>Memories of our lives, of our works and our deeds will continue in others.</p>
+                <h3 className='text-light text-center'>Rosa Parks</h3>
+                <p className='text-light text-center mb-0'>Memories of our lives, of our works and our deeds will continue in others.</p>
               </div>
               <div className='justify-content-end position-absolute bottom-0'>
                 <p className='about-credit-mobile text-light lead fw-medium mb-0'>Photo credit</p>
