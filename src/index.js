ad3be701a5e2584c7bd159a2b3e1f15fea1c8326
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-12-14 12:57:50 +0700

    gpdr cookie preferences init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // React
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import "./components/BurgeonSchema";
@@ -13,6 +13,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 // Styles
 import "./css/style.css";
 import "./css/responsive.css";
+// Cookie Consent
+import * as CookieConsent from "vanilla-cookieconsent";
+import "vanilla-cookieconsent/dist/cookieconsent.css";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
@@ -22,6 +25,63 @@ const TermsConditions = lazy(() => import("./pages/TermsConditions"));
 const helmetContext = {};
 
 function App() {
+  useEffect(() => {
+    CookieConsent.run({
+      categories: {
+        necessary: {
+          enabled: true,
+          readOnly: true,
+        },
+        analytics: {},
+      },
+
+      language: {
+        default: "en",
+        translations: {
+          en: {
+            consentModal: {
+              title: "We use cookies",
+              description: "Cookie modal description",
+              acceptAllBtn: "Accept all",
+              acceptNecessaryBtn: "Reject all",
+              showPreferencesBtn: "Manage Individual preferences",
+            },
+            preferencesModal: {
+              title: "Manage cookie preferences",
+              acceptAllBtn: "Accept all",
+              acceptNecessaryBtn: "Reject all",
+              savePreferencesBtn: "Accept current selection",
+              closeIconLabel: "Close modal",
+              sections: [
+                {
+                  title: "Somebody said ... cookies?",
+                  description: "I want one!",
+                },
+                {
+                  title: "Strictly Necessary cookies",
+                  description:
+                    "These cookies are essential for the proper functioning of the website and cannot be disabled.",
+                  linkedCategory: "necessary",
+                },
+                {
+                  title: "Performance and Analytics",
+                  description:
+                    "These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.",
+                  linkedCategory: "analytics",
+                },
+                {
+                  title: "More information",
+                  description:
+                    'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  }, []);
+
   return (
     <Suspense fallback={<BurgeonPreload />}>
       <BrowserRouter>
