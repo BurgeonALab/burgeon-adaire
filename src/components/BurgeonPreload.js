69528f7bdd09aaaa249fe45cd10636ed1328310a
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2025-04-12 02:03:14 +0700

    fix preload style load

--- src/components/BurgeonPreload.js
+++ src/components/BurgeonPreload.js
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 export default class BurgeonPreload extends Component {
   render() {
     return (
-      <div className="preload">
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000707",
+        }}
+      >
         <img
           src="https://images.burgeonadaire.com/new_bai_img/new_logo.webp"
           alt="PT. Burgeon Adaire International Logo"
