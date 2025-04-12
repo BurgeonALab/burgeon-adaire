0efac0980bd1360488c3fc4662f92b837c2cd089
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2025-04-13 01:48:59 +0700

    delay path

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense, lazy, useEffect, useState } from "react";
+import React, { Fragment, Suspense, useState } from "react";
 import { BurgeonNavbar, BurgeonJumbotron, BurgeonContact } from "../components";
 import { BurgeonSEO } from "../components/BurgeonSEO";
 import { useGSAP } from "@gsap/react";
@@ -15,61 +15,52 @@ import {
 
 export default function HomePage({ currectSection, selectedSection }) {
   const [titlePage, setTitlePage] = useState("");
+  const [isDone, setIsDone] = useState(false);
   gsap.registerPlugin(useGSAP, ScrollToPlugin);
 
   console.log("Current section: " + currectSection);
   console.log("Selected section: " + selectedSection);
 
-  function removeHash(name) {
-    var urlPath = name.toString();
-    var newURL = "/" + urlPath;
-    var urlPathCap = urlPath.charAt(0).toUpperCase() + urlPath.slice(1);
-    var newTitle = urlPathCap + " | PT. Burgeon Adaire International";
-    var newState = {
-      additionalInformation:
-        urlPathCap + " section at PT. Burgeon Adaire International",
-    };
+  const RenderProgress = () => {
+    console.log(isDone);
+    setIsDone(true);
 
-    window.history.pushState(newState, newTitle, newURL);
-  }
-
-  useEffect(() => {
-    if (selectedSection === "Identity") {
-      gsap.to(window, {
-        duration: 0.1,
-        scrollTo: "#abstract",
-      });
-      var name = $(".nav1").html().toLowerCase();
-      removeHash(name);
-      setTitlePage("Identity | PT. Burgeon Adaire International");
-    } else if (selectedSection === "Operations") {
-      gsap.to(window, {
-        duration: 0.1,
-        scrollTo: "#operations",
-      });
-      var name = $(".nav2").html().toLowerCase();
-      removeHash(name);
-      setTitlePage("Operations | PT. Burgeon Adaire International");
-    } else if (selectedSection === "Acknowledgment") {
-      gsap.to(window, {
-        duration: 0.1,
-        scrollTo: "#acknowledgment",
-      });
-      var name = $(".nav3").html().toLowerCase();
-      removeHash(name);
-      setTitlePage("Acknowledgment | PT. Burgeon Adaire International");
-    } else if (selectedSection === "Partners") {
-      gsap.to(window, {
-        duration: 0.1,
-        scrollTo: "#partners",
-      });
-      var name = $(".nav4").html().toLowerCase();
-      removeHash(name);
-      setTitlePage("Partners | PT. Burgeon Adaire International");
-    } else {
-      setTitlePage("PT. Burgeon Adaire International");
+    async function setLoad() {
+      if (isDone == true) {
+        const getToPath = async () => {
+          if (selectedSection === "Identity") {
+            await gsap.to(window, {
+              duration: 0.1,
+              scrollTo: "#abstract",
+            });
+            setTitlePage("Identity | PT. Burgeon Adaire International");
+          } else if (selectedSection === "Operations") {
+            await gsap.to(window, {
+              duration: 0.1,
+              scrollTo: "#operations",
+            });
+            setTitlePage("Operations | PT. Burgeon Adaire International");
+          } else if (selectedSection === "Acknowledgment") {
+            await gsap.to(window, {
+              duration: 0.1,
+              scrollTo: "#acknowledgment",
+            });
+            setTitlePage("Acknowledgment | PT. Burgeon Adaire International");
+          } else if (selectedSection === "Partners") {
+            await gsap.to(window, {
+              duration: 0.1,
+              scrollTo: "#partners",
+            });
+            setTitlePage("Partners | PT. Burgeon Adaire International");
+          } else {
+            setTitlePage("PT. Burgeon Adaire International");
+          }
+        };
+        getToPath();
+      }
     }
-  });
+    setLoad();
+  };
 
   return (
     <Fragment>
@@ -102,6 +93,7 @@ export default function HomePage({ currectSection, selectedSection }) {
           </div>
         </div>
       </div>
+      <RenderProgress />
     </Fragment>
   );
 }
